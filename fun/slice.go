6a22773f6946ceca4b71bcdebd7a13a8c1525b82
hb <hb107@live.cn>
2022-03-30 17:51:49 +0800

    新增函数：去除slice的0值

--- fun/slice.go
+++ fun/slice.go
@@ -1,5 +1,16 @@
 package fun
 
+// ClearZeroSliceInt 去除slice的0值
+func ClearZeroSliceInt(slice []int) []int {
+	var newSlice = make([]int, 0, 0)
+	for _, v := range slice {
+		if v > 0 {
+			newSlice = append(newSlice, v)
+		}
+	}
+	return newSlice
+}
+
 func InSliceInt(value int, slice []int) bool {
 	for _, v := range slice {
 		if v == value {
@@ -18,7 +29,7 @@ func InSliceString(value string, slice []string) bool {
 	return false
 }
 
-//String切片去重
+// SliceUniqueString String切片去重
 func SliceUniqueString(value *[]string) *[]string {
 	result := make([]string, 0, len(*value))
 	temp := map[string]struct{}{}
@@ -31,7 +42,7 @@ func SliceUniqueString(value *[]string) *[]string {
 	return &result
 }
 
-//Int切片去重
+// SliceUniqueInt Int切片去重
 func SliceUniqueInt(value *[]int) *[]int {
 	result := make([]int, 0, len(*value))
 	temp := map[int]struct{}{}
